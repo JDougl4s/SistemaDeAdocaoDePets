c78d28026e36121a48669324fc2c4cbf7796db27
João Douglas <douglasjoao463@gmail.com>
2026-09-09 10:38:05 -0300

    feat: implementação da função de exibir perguntas do formulario

--- src/system/Main.java
+++ src/system/Main.java
@@ -1,7 +1,9 @@
 package system;
 
+import system.files.Formulario;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Formulario.exibirFormulario();
     }
 }
---
+++ src/system/files/Formulario.java
@@ -0,0 +1,22 @@
+package system.files;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Formulario {
+    public static void exibirFormulario(){
+        try (FileReader fileReader = new FileReader("C:\\Users\\newst\\Desktop\\maratona-java-virado-no-jiraya\\SistemasDeCadastros\\src\\system\\formulario.txt\\");
+             BufferedReader bufferedReader = new BufferedReader(fileReader)){
+            String linha;
+
+            while((linha = bufferedReader.readLine()) != null){
+                System.out.println(linha);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
