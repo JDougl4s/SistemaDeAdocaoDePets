900f0e4fec0edb697f9a667fc459913a6bfde2e2
João Douglas <douglasjoao463@gmail.com>
2026-09-09 12:18:54 -0300

    feat: adicionado o menu cli, falta algumas regras

---
+++ src/system/cli/Menu.java
@@ -0,0 +1,41 @@
+package system.cli;
+
+import java.io.Reader;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+
+        while (loop){
+            System.out.println("==================================");
+            System.out.println("            ADOPT-PETs            ");
+            System.out.println("==================================");
+            System.out.println("1 - Cadastrar um novo pet");
+            System.out.println("2 - Alterar os dados do pet cadastrado");
+            System.out.println("3 - Deletar um pet cadastrado");
+            System.out.println("4 - Listar todos os pets cadastrados");
+            System.out.println("5 - Listar pets por algum critério (idade, nome, raça)");
+            System.out.println("6 - Sair");
+            System.out.println("==================================");
+
+            if (!sc.hasNextInt()) {
+                System.out.println("Só aceitamos digitos, escolha novamente");
+            }else{
+                int opcao = sc.nextInt();
+                sc.nextLine();
+
+                if (opcao <= 0 || opcao > 6 ){
+                    System.out.println("Opção inválida, escolha novamente.");
+                }
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    System.out.println("A espera foi interrompida");
+                }
+            }
+        }
+    }
+}
