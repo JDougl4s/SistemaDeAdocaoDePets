a5eaad7e9fe31a220c42808ae27d65d1a93ae461
João Douglas <douglasjoao463@gmail.com>
2026-09-10 11:03:00 -0300

    feat: Criação dos ENUMS TipoPet e Sexo

--- src/system/domain/Sexo.java
+++ src/system/domain/Sexo.java
@@ -1,4 +1,16 @@
 package system.domain;
 
 public enum Sexo {
+    FEMININO("Fêmea"),
+    MASCULINO("Macho");
+
+    private final String sexo;
+
+    Sexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
 }
--- src/system/domain/TipoPet.java
+++ src/system/domain/TipoPet.java
@@ -1,4 +1,17 @@
 package system.domain;
 
 public enum TipoPet {
+    CACHORRO("Cachorro"),
+    GATO("Gato"),
+    PASSARO("Passaro");
+
+    private final String nome;
+
+    TipoPet(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
